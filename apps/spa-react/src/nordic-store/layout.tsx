ec52669650340fa940d621198198d037e25a9fc5
Tobias Belch <tobias.belch@docmorris.de>
2025-08-18 09:20:13 +0200

    chore: reset for starting over

--- apps/spa-react/src/nordic-store/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export function Layout({ children }: PropsWithChildren<unknown>) {
-  return (
-    <main className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
-      {children}
-    </main>
-  );
-}
